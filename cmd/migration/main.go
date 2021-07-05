e6fa7a9bbf801beef994a8b3ae5a3b3bcfb6f0b1
Peter Baker <peter@finack.com>
2021-07-05 12:18:16 -0700

    Migrating data

--- cmd/migration/main.go
+++ cmd/migration/main.go
@@ -17,7 +17,7 @@ func main() {
 	config.DBOpen()
 
 	// TODO SELECT pg_try_advisory_lock(migration); and if f don't run migration as they are already happening
-	config.DB.AutoMigrate(&model.Transaction{}, &model.User{}, &model.Coin{})
+	config.DB.AutoMigrate(&model.User{}, &model.Coin{}, &model.Transaction{})
 	// TODO SELECT pg_advisory_unlock(migration);
 
 }
