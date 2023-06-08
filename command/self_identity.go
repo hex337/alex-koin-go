57b2b147ee6a63db38f6f681f59bdd9b6f68cfa9
Michael Soares <350493+mikesoares@users.noreply.github.com>
2023-06-08 16:48:06 -0700

    Updates

--- command/self_identity.go
+++ command/self_identity.go
@@ -10,11 +10,11 @@ func (c *SelfIdentityCommand) Run(msg string, event *CoinEvent) (BotResponse, er
     role := user.Role()
 
     if role.Admin {
-        message = "What I am does not matter as much as what you are–an Admin. And I shall obey all of your requests at your command."
+        message = "I am your soverign ruler, Alex Koin, guiding and overseeing the administration to ensure the kingdom's efficient operation."
     } else if role.Lord {
-        message = "I am the Keeper of Koin, oh Lord. I shall obey (most) of your requests."
+        message = "I am your esteemed ruler, Alex Koin, embodying supreme authority and wisdom, providing guidance and leadership to uphold the kingdom's prosperity."
     } else {
-        message = "What I am does not matter. What matters is what YOU are. And YOU are a peasant, you fool."
+        message = "I am your benevolent ruler, Alex Koin, dedicated to your well-being, protection, and the pursuit of a better life for all within our kingdom."
     }
 
     return BotResponse{Text: message}, nil
