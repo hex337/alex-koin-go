473175951f3a3d77724e393c23cc402170db5d9f
Michael Soares <350493+mikesoares@users.noreply.github.com>
2023-06-08 16:48:44 -0700

    Typo

--- command/self_identity.go
+++ command/self_identity.go
@@ -10,7 +10,7 @@ func (c *SelfIdentityCommand) Run(msg string, event *CoinEvent) (BotResponse, er
     role := user.Role()
 
     if role.Admin {
-        message = "I am your soverign ruler, Alex Koin, guiding and overseeing the administration to ensure the kingdom's efficient operation."
+        message = "I am your sovereign ruler, Alex Koin, guiding and overseeing the administration to ensure the kingdom's efficient operation."
     } else if role.Lord {
         message = "I am your esteemed ruler, Alex Koin, embodying supreme authority and wisdom, providing guidance and leadership to uphold the kingdom's prosperity."
     } else {
