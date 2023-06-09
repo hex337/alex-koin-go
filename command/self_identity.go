a1560d6f9976feed2f7802c88d5352225d3c7479
Michael Soares <350493+mikesoares@users.noreply.github.com>
2023-06-09 11:10:24 -0700

    Removing an errant space that Ziad added (#30)

--- command/self_identity.go
+++ command/self_identity.go
@@ -12,7 +12,7 @@ func (c *SelfIdentityCommand) Run(msg string, event *CoinEvent) (BotResponse, er
     if role.Admin {
         message = "I am your sovereign ruler, Alex Koin, guiding and overseeing the administration to ensure the kingdom's efficient operation. You are always welcome at my table."
     } else if role.Lord {
-        message = "I am your esteemed ruler, Alex Koin, embodying supreme authority and wisdom, providing guidance and leadership to uphold the kingdom's prosperity.  Blessings onto you, my cherub."
+        message = "I am your esteemed ruler, Alex Koin, embodying supreme authority and wisdom, providing guidance and leadership to uphold the kingdom's prosperity. Blessings onto you, my cherub."
     } else {
         message = "I am your benevolent ruler, Alex Koin. I bring joy to your life. Never forget to honor your lords."
     }
